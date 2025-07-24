44b0d726ebb58ea4bfa44b00be452c4d893156a8
Md. Suhag Mia <mdshohagmia53200@gmail.com>
2025-07-24 11:16:37 +0600

    a

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from './Container'
+
+const Footer = () => {
+  return (
+    <footer className='bg-[#000000] text-white py-4 flex justify-center items-center'>
+      <Container>
+        <div>
+          <h1 className='text-6xl font-bold font-mono'>Footer</h1>
+        </div>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/root/RootLayout.jsx
+++ src/components/root/RootLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Menu from '../Menu'
 import { Outlet } from 'react-router-dom'
+import Footer from '../Footer'
 
 const RootLayout = () => {
   return (
     <>
      <Menu/>
      <Outlet/>
+     <Footer/>
     </>
   )
 }
